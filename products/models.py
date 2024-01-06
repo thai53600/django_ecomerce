7a198feaba34761d1b458d4f80bfa31520d06561
Nguyen Chau Quyen <quyen46919@donga.edu.vn>
2024-01-06 17:22:13 +0700

    feat: create product-features models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    slug = models.CharField(max_length=255)
+    icon_url = models.CharField(max_length=128, null=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True)
+
+
+class Product(models.Model):
+    class Unit(models.TextChoices):
+        VND = 'VND', 'Viet Nam Dong'
+        USD = 'USD', 'United States Dollar'
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    unit = models.CharField(max_length=3, choices=Unit.choices)
+    price = models.FloatField()
+    discount = models.IntegerField()
+    amount = models.IntegerField()
+    is_public = models.BooleanField()
+    thumbnail = models.CharField(max_length=128)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', null=False)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        indexes = [
+            models.Index(fields=['-created_at'])
+        ]
+
+    def __str__(self):
+        return self.id
+
+
+class ProductImage(models.Model):
+    id = models.AutoField(primary_key=True)
+    image_url = models.CharField(max_length=128)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_images', null=False)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True)
+
+
+class ProductComment(models.Model):
+    id = models.AutoField(primary_key=True)
+    rating = models.IntegerField()
+    comment = models.CharField(max_length=512)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_comments', null=False)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    parent_id = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True)
+
